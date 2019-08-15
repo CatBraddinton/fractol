ee45064a1ec6c697e9308596e2aa0446adea4b38
Kateryna DUDKO <katrinmclawren@gmail.com>
2019-08-15 20:05:09 +0300

    modifies functions to use t_data *data struct, and to use new size vars instead of defined one

--- src/mandelbrot_set.c
+++ src/mandelbrot_set.c
@@ -12,22 +12,23 @@
 
 #include "../inc/fractol.h"
 
-void	draw_mandelbrot_set(void)
+void	draw_mandelbrot_set(t_data *data)
 {
-	t_mlx	data;
-
-	if ((data.p_mlx = mlx_init()) == NULL)
+	if ((data->mlx_data->p_mlx = mlx_init()) == NULL)
 		error("Error: mlx init failed in draw_mandelbrot_set()");
-	if ((data.p_window = mlx_new_window(data.p_mlx, WIN_WIDTH, WIN_HEIGHT,
-										"Mandelbrot Set")) == NULL)
+	if (!(data->mlx_data->p_window = mlx_new_window(data->mlx_data->p_mlx,
+		data->win_width, data->win_height,
+		"Mandelbrot Set")))
 		error("Error: mlx failed to open new window in draw_mandelbrot_set()");
-	if (!(data.p_image = mlx_new_image(data.p_mlx, IMG_WIDTH, IMG_HEIGHT)))
+	if (!(data->mlx_data->p_image = mlx_new_image(data->mlx_data->p_mlx,
+		data->img_width, data->img_height)))
 		error("Error: mlx failed to create new image in draw_mandelbrot_set()");
-	if ((data.img_buffer = mlx_get_data_addr(data.p_image, &(data.bpp),
-									&(data.size_line), &(data.endian))) == NULL)
+	if ((data->mlx_data->img_buffer = mlx_get_data_addr(data->mlx_data->p_image,
+		 &(data->mlx_data->bpp), &(data->mlx_data->size_line),
+		 &(data->mlx_data->endian))) == NULL)
 		error("Error: mlx failed to return information about created image\
 		in draw_mandelbrot_set()");
-	mlx_hook(data.p_window, 2, 0, key_press, &data);
-	mlx_hook(data.p_window, 17, 0, close, &data);
-	mlx_loop(data.p_mlx);
+	mlx_hook(data->mlx_data->p_window, 2, 0, key_press, &data);
+	mlx_hook(data->mlx_data->p_window, 17, 0, close, &data);
+	mlx_loop(data->mlx_data->p_mlx);
 }
