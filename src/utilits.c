4a42e10720aa7447a67c3f524ef99fc0d3f17103
Kateryna DUDKO <katrinmclawren@gmail.com>
2019-08-15 20:04:17 +0300

    adds init_data(), it allocates memory for mlx structure and initializate variables to zeros. size vars are initializated to specific size base on number of windows

--- src/utilits.c
+++ src/utilits.c
@@ -12,13 +12,31 @@
 
 #include "../inc/fractol.h"
 
-void	count_fractal(int type)
+void	init_data(t_data *all_data, char *input)
 {
-	void (*draw_fractal[TOTAL_NB + 1]) (void);
+	if ((all_data->mlx_data = (t_mlx *)malloc(sizeof(t_mlx))) == NULL)
+		error("Error: malloc failed in init_data()");
+	all_data->type = get_fractal_type(input);
+	all_data->win_width = WIN_WIDTH / all_data->windows_count;
+	all_data->win_height = WIN_HEIGHT / all_data->windows_count;
+	all_data->img_width = IMG_WIDTH / all_data->windows_count;
+	all_data->img_height = IMG_HEIGHT / all_data->windows_count;
+	all_data->mlx_data->p_mlx = NULL;
+	all_data->mlx_data->p_window = NULL;
+	all_data->mlx_data->p_image = NULL;
+	all_data->mlx_data->img_buffer = NULL;
+	all_data->mlx_data->bpp = 0;
+	all_data->mlx_data->size_line = 0;
+	all_data->mlx_data->endian = 0;
+}
+
+void	count_fractal(t_data *data)
+{
+	void (*draw_fractal[TOTAL_NB + 1]) (t_data *data);
 
 	draw_fractal[0] = NULL;
 	draw_fractal[JULIA] = draw_julia_set;
 	draw_fractal[MANDELBROT] = draw_mandelbrot_set;
 	draw_fractal[SIERPINSKI] = draw_sierpinskie_triangle;
-	(*draw_fractal[type])();
+	(*draw_fractal[data->type])(data);
 }
