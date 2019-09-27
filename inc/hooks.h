2dcc434657025952cb47497e45da5a9f155ee746
Kateryna DUDKO <katrinmclawren@gmail.com>
2019-09-27 07:06:32 +0300

    now defines all mac keyboard keys

--- inc/hooks.h
+++ inc/hooks.h
@@ -13,57 +13,130 @@
 #ifndef HOOKS_H
 # define HOOKS_H
 
-// # define DEL		117
-// # define SPACE 		49
-# define ESC		53
-//
-# define ZOOM_P		116
-# define ZOOM_M		121
-// # define PLUS		27
-// # define MINUS		24
-# define UP			126
-# define DOWN		125
-# define LEFT		123
-# define RIGHT		124
-// # define Z_UP		79
-// # define Z_DOWN		68
-// # define ROT_XP		12
-// # define ROT_XM		14
-// # define ROT_YP		0
-// # define ROT_YM		2
-// # define ROT_ZP		6
-// # define ROT_ZM		8
-// # define I			34
-// # define P			35
-//
-// # define ONE		18
-// # define TWO		19
-// # define THREE		20
-// # define FOUR		21
-// # define FIVE		23
-// # define SIX		22
-// # define SEVEN		26
-// # define EIGHT		28
-// # define NINE		25
-// # define ZERO		29
-//
-// # define NUM_ONE	83
-// # define NUM_TWO	84
-// # define NUM_THREE	85
-// # define NUM_FOUR	86
-// # define NUM_FIVE	87
-// # define NUM_SIX	88
-// # define NUM_SEVEN	89
-// # define NUM_EIGHT	91
-// # define NUM_NINE	92
-// # define NUM_ZERO	82
+# define KEY_ESC		53
+# define KEY_BACKSPACE	51
+# define KEY_TAB		48
+# define KEY_CAPSLOCK	272
+# define KEY_L_SHIFT	257
+# define KEY_L_CTRL		256
+# define KEY_L_ALT		261
+# define KEY_L_CMD		259
+# define KEY_SPACE 		49
+# define KEY_R_CMD		260
+# define KEY_R_ALT		262
+# define KEY_R_CTRL		269
+# define KEY_R_SHIFT	258
+# define KEY_ENTER		36
 
-# define MOUSE_LEFT			1
-# define MOUSE_RIGHT		2
-# define MOUSE_MIDDLE		3
-# define MOUSE_SCROLL_DOWN	4
-# define MOUSE_SCROLL_UP	5
-# define MOUSE_SCROLL_LEFT	6
-# define MOUSE_SCROLL_RIGHT	7
+# define KEY_F1			122
+# define KEY_F2			120
+# define KEY_F3			99
+# define KEY_F4			118
+# define KEY_F5			96
+# define KEY_F6			97
+# define KEY_F7			98
+# define KEY_F8			100
+# define KEY_F9			101
+# define KEY_F10		109
+# define KEY_F11		103
+# define KEY_F12		111
+# define KEY_F13		105
+# define KEY_F14		107
+# define KEY_F15		113
+# define KEY_F16		106
+# define KEY_F17		64
+# define KEY_F18		79
+# define KEY_F19		80
+
+# define KEY_MINUS_PLUS	10
+
+# define KEY_ONE		18
+# define KEY_TWO		19
+# define KEY_THREE		20
+# define KEY_FOUR		21
+# define KEY_FIVE		23
+# define KEY_SIX		22
+# define KEY_SEVEN		26
+# define KEY_EIGHT		28
+# define KEY_NINE		25
+# define KEY_ZERO		29
+# define KEY_MINUS		27
+# define KEY_PLUS		24
+
+# define KEY_Q			12
+# define KEY_W			13
+# define KEY_E			14
+# define KEY_R			15
+# define KEY_T			17
+# define KEY_Y			16
+# define KEY_U			32
+# define KEY_I			34
+# define KEY_O			31
+# define KEY_P			35
+# define KEY_L_BRACKET	33
+# define KEY_R_BRACKET	30
+# define KEY_A			0
+# define KEY_S			1
+# define KEY_D			2
+# define KEY_F			3
+# define KEY_G			5
+# define KEY_H			4
+# define KEY_J			38
+# define KEY_K			40
+# define KEY_L			37
+# define KEY_SEMICOLON	41
+# define KEY_QUOTS		39
+# define KEY_BACKSLASH	42
+# define KEY_APPROX		50
+# define KEY_Z			6
+# define KEY_X			7
+# define KEY_C			8
+# define KEY_V			9
+# define KEY_B			11
+# define KEY_N			45
+# define KEY_M			46
+# define KEY_LESS_THAN	43
+# define KEY_GRATER_THAN 	47
+# define KEY_SLASH		44
+
+# define KEY_FN			279
+# define KEY_HOME		115
+# define KEY_END		119
+# define KEY_PG_UP		116
+# define KEY_PG_DOWN	121
+# define KEY_DEL		117
+
+# define KEY_UP			126
+# define KEY_DOWN		125
+# define KEY_LEFT		123
+# define KEY_RIGHT		124
+
+# define KEY_NUM_ONE	83
+# define KEY_NUM_TWO	84
+# define KEY_NUM_THREE	85
+# define KEY_NUM_FOUR	86
+# define KEY_NUM_FIVE	87
+# define KEY_NUM_SIX	88
+# define KEY_NUM_SEVEN	89
+# define KEY_NUM_EIGHT	91
+# define KEY_NUM_NINE	92
+# define KEY_NUM_ZERO	82
+
+# define KEY_NUM_CLEAR	71
+# define KEY_NUM_EQUAL	81
+# define KEY_NUM_SLASH	75
+# define KEY_NUM_MULTIPLY	67
+# define KEY_NUM_MINUS	78
+# define KEY_NUM_PLUS	69
+# define KEY_NUM_ENTER	76
+# define KEY_NUM_POINT	65
+
+# define KEY_MOUSE_LEFT			1
+# define KEY_MOUSE_RIGHT		2
+# define KEY_MOUSE_MIDDLE		3
+# define KEY_MOUSE_SCROLL_DOWN	4
+# define KEY_MOUSE_SCROLL_UP	5
+# define KEY_MOUSE_SCROLL_LEFT	6
+# define KEY_MOUSE_SCROLL_RIGHT	7
 
 #endif
